29ca1271faaab7a3879d897142e3a82e736c65cb
Brienne Kordis <brienne@megaphonetech.com>
2022-09-14 11:39:51 -0400

    create Presentation Entity

---
+++ CRM/Presentation/BAO/Presentation.php
@@ -0,0 +1,26 @@
+<?php
+use CRM_Presentation_ExtensionUtil as E;
+
+class CRM_Presentation_BAO_Presentation extends CRM_Presentation_DAO_Presentation {
+
+  /**
+   * Create a new Presentation based on array-data
+   *
+   * @param array $params key-value pairs
+   * @return CRM_Presentation_DAO_Presentation|NULL
+   *
+  public static function create($params) {
+    $className = 'CRM_Presentation_DAO_Presentation';
+    $entityName = 'Presentation';
+    $hook = empty($params['id']) ? 'create' : 'edit';
+
+    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+    $instance = new $className();
+    $instance->copyValues($params);
+    $instance->save();
+    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
+
+    return $instance;
+  } */
+
+}
